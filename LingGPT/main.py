f0294320c86856965d920a6dc7621481b59f0799
Guy Mor-Lan <etherx@huji-132-64-29-166.xt.huji.ac.il>
2023-06-08 14:27:01 +0300

    print port

--- LingGPT/main.py
+++ LingGPT/main.py
@@ -2,4 +2,6 @@ from .linggpt import app, socketio
 
 def main():
     socketio.run(app, debug=True, port=8000)
+    print("Server running on port 8000")
+    print("Open http://localhost:8000 in your browser")
 
